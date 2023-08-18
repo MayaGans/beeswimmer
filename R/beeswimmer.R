6e3e3e17751e738e87dea206697dcc518ed5ec46
Jiwan Heo <jiwanheo123@gmail.com>
2023-08-18 10:02:23 -0400

    get rid of browser

--- R/beeswimmer.R
+++ R/beeswimmer.R
@@ -11,7 +11,6 @@ beeswimmer <- function(data, unique_alert_cat = NULL, overall_view = FALSE, widt
   if (x_is_avisit) {
     x_domain <- levels(data$timing)
   } else {
-    browser()
     # Make sure last_collected is part of the x scale, if it is larger than the bubbles' x
     # Throwing it in min also, JUST in case
     last_collected  <- unique(data$last_collected)
